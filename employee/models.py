944641cfd3359ce9c84c1befaaa744a225e5de75
Hassan <hassan@gmail.com>
2021-12-05 03:35:56 +0500

    working com on created and updated date now working on createdby

--- employee/models.py
+++ employee/models.py
@@ -15,5 +15,7 @@ class Employee(models.Model):
     mobile = models.CharField(max_length=105)
     position = models.CharField(max_length=105)
 
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now = True)
     def __str__(self):
         return self.fullname
